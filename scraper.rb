aa70ee13872a67cfacb5dd377511b57446d16924
Tony Bowden <tony@tmtm.com>
2019-02-03 20:00:00 +0000

    Also include existing matches in EP

--- scraper.rb
+++ scraper.rb
@@ -1,8 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/pontifical-commission-for-vatican-state-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+epmembers = EveryPolitician::Index.new.country("Vatican-City").lower_house.popolo.persons.map(&:wikidata).compact
+wikipedia = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/pontifical-commission-for-vatican-state-wikipedia', column: 'id')
 
+EveryPolitician::Wikidata.scrape_wikidata(ids: epmembers | wikipedia)
